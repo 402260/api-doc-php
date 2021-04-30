70018cfdb766da26e4d953045b1e9da707718bb5
phpwdk <9653992@qq.com>
2021-04-30 21:56:21 +0800

    修复问题

--- src/BootstrapApiDoc.php
+++ src/BootstrapApiDoc.php
@@ -122,7 +122,7 @@ class BootstrapApiDoc extends ApiDoc
         </head>
         <body>
         <div class="container-fluid">
-             <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top navbar-light bg-light">
+             <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top navbar-light bg-light">
                    <a class="navbar-brand" href="#">API文档</a>
                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" >
                        <span class="navbar-toggler-icon"></span>
